8e0d257058aab5eb5bd8636a85b0743de11029de
inoxx03 <inoxxenator@gmail.com>
2016-11-25 23:00:24 +0100

    added rudimentary text  interface

--- morsecodify/morsecodify.py
+++ morsecodify/morsecodify.py
@@ -1,5 +1,7 @@
 # python 3.4.2
 # Morse Code Dictionary
+# make this able to process a dictionary from separate txt file
+# TODO: add support for US, Continental (requires non-latin chars) and ITU MC varieties
 CODE = {'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..', 'E': '.',
           'F': '..-.',  'G': '--.',   'H': '....',  'I': '..',  'J': '.---', 
           'K': '-.-',   'L': '.-..',  'M': '--',    'N': '-.',  'O': '---', 
@@ -12,14 +14,14 @@ CODE = {'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..', 'E': '.',
           '6': '-....',  '7': '--...',  '8': '---..',
           '9': '----.',
 
-          ' ':'  ', '.':'.-.-.-', ',':'--..--',
+          ' ':' ', '.':'.-.-.-', ',':'--..--',
           ':':'---...', '?':'..--..', "'":'.----.',
           '-':'-....-', '/':'-..-.', '@': '.--.-.',
           '=':'-...-', '(':'-.--.', ')':'-.--.-',
           '+':'.-.-.', '!': '-.-.--'
     }
 
-print ("Welcome to Morsecodify, the simple text to morse code translator")
+print ("Welcome to Morsecodify v0.1 - the simple text-to-morse code translator powered by Python v3")
 
 #function that converts text to morse code    
 def morsecodify(msg):
@@ -30,7 +32,10 @@ def morsecodify(msg):
       morse = morse + CODE[character] + " " # add to converted message + include space after char
   return morse
 
-msg = input("input message: ")
+msg = input("Input Text: ")
 morse = morsecodify(msg)
+print("Your Message in Morse Code:")
 print("/MSG START/ " + morse + "/MSG END/")
+
+# code here to choose if quit or another input
 
