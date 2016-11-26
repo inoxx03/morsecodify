a9bb7d19eb03ec7878e99ea766c1f536a15c4524
inoxx03 <inoxxenator@gmail.com>
2016-11-26 09:02:35 +0100

    added main menu loop w/ 2 options

--- morsecodify/morsecodify.py
+++ morsecodify/morsecodify.py
@@ -21,21 +21,49 @@ CODE = {'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..', 'E': '.',
           '+':'.-.-.', '!': '-.-.--'
     }
 
-print ("Welcome to Morsecodify v0.1 - the simple text-to-morse code translator powered by Python v3")
-
 #function that converts text to morse code    
 def morsecodify(msg):
   msg = msg.upper() #convert message to uppercase
-  morse = ""
+  morse = "" # dump characters here before print
   # convert each character in the message to morse 
   for character in msg:
       morse = morse + CODE[character] + " " # add to converted message + include space after char
   return morse
 
-msg = input("Input Text: ")
-morse = morsecodify(msg)
-print("Your Message in Morse Code:")
-print("/MSG START/ " + morse + "/MSG END/")
+print ("Welcome to Morsecodify v0.1 - the simple text-to-morse code translator powered by Python v3")
 
+print ('What would you like to do?\nType:\n(c) - Convert text to Morse code\n(e) - Exit')
+
+# main menu
+m0 = input('Select option and press ENTER: ')
+
+def validate_choice(m0):
+  if m0 == 'c':
+      return True
+  elif m0 == 'e':
+      return True
+  return False
+
+while True:
+  try:
+      m0 = str(input("Select option: "))
+      if validate_choice(m0): break
+  except ValueError:
+      print ("Invalid option!")
+
+# make this go back to taking input if no valid input is given
+if m0 == 'c':
+  msg = input("Input Text: ")
+  morse = morsecodify(msg)
+  print("Your Message in Morse Code:")
+  print("/MSG START/ " + morse + "/MSG END/")
+elif m0 == 'e':
+  print('Bye!')
+  # give option to clear screen
+  exit()
+else:
+  print(m0 + " is not a valid option!")
+  m0 = input("Select a valid option: ")
+  
 # code here to choose if quit or another input
 
